8a2c93004c43c6d03329deb72d5518dbbee9b03e
Dominic Go <dominicstop@gmail.com>
2020-06-26 01:44:43 +0800

    Migrated to use swift as the main entry point
    Summary: Migrated to use swift as the main entry point for the app instead of Obj-C (i.e replaced `main.m`, `AppDelegate.h`, and `AppDelegate.m` with `AppDelegate.swift`)

--- ios/nativeUIModulesTest-Bridging-Header.h
+++ ios/nativeUIModulesTest-Bridging-Header.h
@@ -1,17 +1,22 @@
 
 
-//#import <React/RCTBundleURLProvider.h>
-//#import <React/RCTRootView.h>
-//#import <React/RCTComponent.h>
-//#import <React/RCTBridgeModule.h>
-#import <React/RCTViewManager.h>
+#if DEBUG
+#import <React/RCTDevLoadingView.h>
+#endif
+
 #import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
+#import <React/RCTConvert.h>
+#import <React/RCTBundleURLProvider.h>
+
+#import <React/RCTViewManager.h>
 #import <React/RCTUIManager.h>
 #import <React/RCTTouchHandler.h>
 #import <React/RCTAssert.h>
-//#import <React/RCTDevLoadingView.h>
 #import <React/UIView+React.h>
 #import <React/RCTTextView.h>
 #import <React/RCTTextShadowView.h>
 #import <React/RCTComponent.h>
-//#import <React/RCTConvert.h>
